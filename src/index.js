3fb3f789efa9d638a7e08a704dc62e2d288b6830
해모리 <ducktack@sindonghyeon-ui-MacBookAir.local>
2026-09-23 18:12:43 +0900

    feat: 색상규칙과 랜덤색상함수 생성

--- src/index.js
+++ src/index.js
@@ -1,10 +1,48 @@
 const INITIAL_MONEY = 10000; //게임 시작 시 플레이어는 초기 자금 10,000원을 가진다.
+const INITIAL_ROUND = 0;
+const COLORS = [
+  //확률을 백분율*10으로 저장
+  { name: 'YELLOW', probability: 525, multiplier: 1 },
+  { name: 'GREEN', probability: 250, multiplier: 3 },
+  { name: 'BLUE', probability: 150, multiplier: 5 },
+  { name: 'PURPLE', probability: 50, multiplier: 10 },
+  { name: 'RED', probability: 25, multiplier: 20 },
+];
 
 export default class RouletteGame {
   constructor() {
     this.money = INITIAL_MONEY;
+    this.round = INITIAL_ROUND;
   }
-  play(colorSelect, betAmount) {}
+  play(color, betAmount) {
+    if (!this.isValid(color, betAmount)) return '';
+  }
+  isValid(color, betAmount) {
+    if (color === '') {
+      return false;
+    }
+    if (betAmount <= 0) {
+      return false;
+    }
+    if (this.money < betAmount) {
+      return false;
+    }
+    return true;
+  }
+  makeComputerColor() {
+    const randomNumber = Math.floor(Math.random() * 1000) + 1;
+    return this.convertNumberToColor(randomNumber);
+  }
+  convertNumberToColor(number) {
+    let accumulatedProbability = 0;
+    for (const color of COLORS) {
+      accumulatedProbability += color.probability;
+      if (number <= accumulatedProbability) return color.name;
+    }
+  }
+  addRound() {
+    this.round++;
+  }
 }
 
 const moneyElement = document.getElementById('current-money');
